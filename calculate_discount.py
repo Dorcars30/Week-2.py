07b4cf926557a81389f6b4ebac92ff0fccb9146d
Dorcars Soladoye <dorcarssoladoye@gmail.com>
2025-08-29 21:43:37 +0100

    initial commit

---
+++ calculate_discount.py
@@ -0,0 +1,14 @@
+```python
+def calculate_discount(20,000, 20):
+    if >= 20:
+= 20,000* (20 / 100)
+        return price - discount_amount
+    else:
+        return price
+float(input("20,000:"))
+float(input("20: "))
+
+(20,000, 20)
+
+print("16,000:",)
+```
