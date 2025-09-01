33b1a9ee644fb3c4e3bdf7493d089a8dd4f8f0ae
Dorcars Soladoye <dorcarssoladoye@gmail.com>
2025-09-01 10:01:38 +0100

    initial commit

---
+++ class_challenge.py
@@ -0,0 +1,10 @@
+class_challenge=()
+class Plane(Vehicle):
+    def move(self):
+        print("The plane is flying in the sky.")
+
+def vehicle_movement(vehicle: vehicle):
+    plane.fly()
+
+vehicle_movement(my_plane)
+
